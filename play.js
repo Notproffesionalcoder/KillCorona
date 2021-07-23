a132de47339419163e376f581bbfa55ebc46e315
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-07-23 12:49:21 +0530

    Create play.js

---
+++ play.js
@@ -0,0 +1,10 @@
+"use strict"
+		var stage = {
+			w:1280,
+			h:720
+		}
+
+		var _pexcanvas = document.getElementById("canvas");
+		_pexcanvas.width = stage.w;
+		_pexcanvas.height = stage.h;
+		var ctx = _pexcanvas.getContext("2d");
